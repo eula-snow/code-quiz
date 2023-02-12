3302f6cfda89777af4783c2e672634ea53afd0cd
Maria Konova <arim.novak@gmail.com>
2023-02-12 18:46:16 +0000

    adds questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+var questions = [
+  {
+    question: "Which of these data types is considered complex?",
+    answer1: "1. Boolean",
+    answer2: "2. String",
+    answer3: "3. Object",
+    answer4: "4. Symbol",
+    correct: "three",
+  },
+  {
+    question: "Which method can be used to add items to the end of an array?",
+    answer1: "1. arr.pop()",
+    answer2: "2. arr.push()",
+    answer3: "3. arr.shift()",
+    answer4: "4. arr.unshift()",
+    correct: "two",
+  },
+  {
+    question:
+      "Which loop will run at least once whether or not its condition evaluates to 'true'?",
+    answer1: "1. For",
+    answer2: "2. While",
+    answer3: "3. For...of",
+    answer4: "4. Do...while",
+    correct: "four",
+  },
+  {
+    question: "What is the correct syntax for an arrow function?",
+    answer1: "1. const func = () => {a + b}",
+    answer2: "2. const func() => {a + b}",
+    answer3: "3. const func() = {a + b}",
+    answer4: "4. const funct => () = {a + b}",
+    correct: "one",
+  },
+  {
+    question: `What is the correct way to select <h1 class = 'title'>Title</h1>using JavaScript?`,
+    answer1: "1. document.querySelector(h1)",
+    answer2: "2. document.querySelector('#title')",
+    answer3: "3. document.getElementById('title')",
+    answer4: "4. document.querySelector('.title')",
+    correct: "four",
+  },
+];
